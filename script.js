f43e98f85d00d0bbbc63272627651d71bf766593
Kyle Angelich <kyleangelich1@gmail.com>
2023-07-06 19:30:11 -0700

    Add operate function

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 // Never use eval() function.
 // Dont return a new Function() that evaluates a string either (potential fo rinsecure data)
 
-let firstNumber = "";
-let secondNumber = "";
+let firstNumber = 10;
+let secondNumber = 3;
 let operator = ["+", "-", "*", "/"];
 
 function addVariables(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 }
-// console.log(addVariables(a, b));
+// console.log(addVariables(firstNumber, secondNumber));
 
 function subtractVariables(firstNumber, secondNumber) {
     return firstNumber - secondNumber;
 }
-// console.log(subtractVariables(a, b));
+// console.log(subtractVariables(firstNumber, secondNumber));
 
 function multiplyVariables(firstNumber, secondNumber) {
     return firstNumber * secondNumber;
 }
-// console.log(multiplyVariables(a, b));
+// console.log(multiplyVariables(firstNumber, secondNumber));
 
 function divideVariables(firstNumber, secondNumber) {
     return firstNumber / secondNumber;
 }
-// console.log(divideVariables(a, b));
+// console.log(divideVariables(firstNumber, secondNumber));
+
+function operate(firstNumber, secondNumber) {
+    let operator = "+";
+
+    if (operator === "+") {
+        return addVariables(firstNumber, secondNumber);
+    } else if (operator === "-") {
+        return subtractVariables(firstNumber, secondNumber);
+    } else if (operator === "*") {
+        return multiplyVariables(firstNumber, secondNumber);
+    } else {
+        return divideVariables(firstNumber, secondNumber);
+    }
+}
+console.log(operate(firstNumber, secondNumber));
