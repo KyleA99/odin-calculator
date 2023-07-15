0c28252f221a753e2e79f5ccf091d8bd02c5a2b2
Kyle Angelich <kyleangelich1@gmail.com>
2023-07-14 21:48:47 -0700

    Add comment documentation for arithmetic functions

--- script.js
+++ script.js
@@ -5,21 +5,45 @@ let firstNumber = 10;
 let secondNumber = 3;
 let operator = ["+", "-", "*", "/"];
 
+/**
+ * Adds two numbers together and returns the result.
+ * @param {number} firstNumber - The first number to be added.
+ * @param {number} secondNumber - The second number to be added.
+ * @returns {number} The sum of the two numbers.
+ */
 function addVariables(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 }
 // console.log(addVariables(firstNumber, secondNumber));
 
+/**
+ * Subtracts the second number from the first number and returns the result.
+ * @param {number} firstNumber -  The number from which the second number will be subtracted.
+ * @param {number} secondNumber - The number to be subtracted.
+ * @returns {number} The result of subtracting the second number from the first number.
+ */
 function subtractVariables(firstNumber, secondNumber) {
     return firstNumber - secondNumber;
 }
 // console.log(subtractVariables(firstNumber, secondNumber));
 
+/**
+ * Multiplies two numbers together and returns the result.
+ * @param {*} firstNumber - The first number to be multiplied.
+ * @param {*} secondNumber - The second number to be multiplied.
+ * @returns {number} The product of the two numbers.
+ */
 function multiplyVariables(firstNumber, secondNumber) {
     return firstNumber * secondNumber;
 }
 // console.log(multiplyVariables(firstNumber, secondNumber));
 
+/**
+ * Divides the first number by the second number and returns the result.
+ * @param {*} firstNumber - The number to be divided.
+ * @param {*} secondNumber - The number by which the first number will be divided.
+ * @returns {number} The result of dividing the first number by the second number.
+ */
 function divideVariables(firstNumber, secondNumber) {
     return firstNumber / secondNumber;
 }
@@ -92,3 +116,6 @@ function displayVariables() {
     });
 }
 displayVariables();
+
+// Need to call the value of displayVariables() into DOM and display in .screen.
+// This displayed-value in screen should be stored in a variable for future use.
